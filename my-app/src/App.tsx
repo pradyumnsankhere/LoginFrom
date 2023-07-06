abc57beb93cd3f45f6dde2d399ee65580508c71c
prafullbankey <prafull.bakey@preciousinfosystem.com>
2023-07-06 11:34:34 +0530

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import JobApp from './Form/JobApp';
+
+function App() {
+  return (
+    <div>
+      <JobApp/>
+    </div>
+  );
+}
+
+export default App;
